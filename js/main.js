233df881a49985eb2d2aaa0c857ebe81c0100e90
Aleksandra Khrapkova <axi83@mail.ru>
2022-10-24 13:09:21 +0300

    изменяет функцию получения рандомного числа

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-function getRandomIntegerNumber(min, max) {
-  if (isNaN(Number(min)) || isNaN(Number(max)) || min < 0 || min >= max) {
-    throw new Error('Задан неверный диапазон');
-  }
+function getRandomIntegerNumber(a, b) {
+  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-getRandomIntegerNumber(3, 10);
 
 function checkMaxLength(str, maxLength) {
   return str.length <= maxLength;
 }
 
+getRandomIntegerNumber(0, 3);
 checkMaxLength('test', 10);
